d555b4c36a92b40e46760afa8e21c041101aee9e
Liyuan Tan <dennisyuan0219@gmail.com>
2023-06-09 00:02:21 +0800

    modify

--- art101/personal/js/lab.js
+++ art101/personal/js/lab.js
@@ -4,30 +4,6 @@
  * Project-Personal Page
  */
 
-function getRandomColor() {
-    var letters = "0123456789ABCDEF";
-    var color = "#";
-    for (var i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-}
-
-// Function to change the page colors
-function changeColor() {
-    var body = $("body");
-
-    // Generate random colors
-    var bgColor = getRandomColor();
-    var fontColor = getRandomColor();
-
-    // Apply the new colors
-    body.css({ "backgroundColor": bgColor, "color": fontColor });
-}
-
-// Change colors every 3 seconds
-setInterval(changeColor, 3000);
-
 // Question reply
 $("#btn").click(function () {
     var value = $("#input")[0].value;
