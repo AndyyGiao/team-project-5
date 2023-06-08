25835664abf48f50cfca32f1ba3029698195e332
Liyuan Tan <dennisyuan0219@gmail.com>
2023-06-09 00:30:24 +0800

    lab5

---
+++ art101/lab5/js/lab.js
@@ -0,0 +1,22 @@
+/**
+ * Author:    Liyuan Tan
+ * Created:   09.06.2023
+ **/
+
+// define
+make = "Ford";
+model = "Taurus";
+color = "Rusty";
+year = 1996;
+
+// cal
+age = 2023 - year;
+
+// output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + "<br>");
+
+
